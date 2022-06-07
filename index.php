ba7f777041942839e949c79229f197bfaa99dfc8
Ninaxtrem <nina@starxtrem.com>
2022-06-07 15:45:53 +0200

    section1

--- index.php
+++ index.php
@@ -7,44 +7,76 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-        <link rel="stylesheet" type="text/css" href="./assets/css/style.css" />
-        <link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Graduate&display=swap" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="./assets/css/style.css" />
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Graduate&display=swap" rel="stylesheet">
     <title>Examen</title>
 </head>
 
 <body>
 
-<!--NavBarre-->
-<div class="sticky-top">
-    <nav class="navbar navbar-dark bg-dark">
-      <div class="container justify-content-between">
-        <div class="d-none d-lg-block">
-        <a href="./index.php">
-          <img src="assets/img/2.png" alt="" width="90" height="90">
-          </a>
-        </div>
-        <div class="navbar navbar-expand-lg">
-          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar-content"
-            aria-controls="navbar-content" aria-expanded="false" aria-label="toggle-navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbar-content">
-            <ul class="navbar-nav">
-              <li class="nav-item">
-                <a class="nav-link" href="./index.php">Accueil</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="">Projets</a>
-              </li>
-            </ul>
-          </div>
-        </div>
-    </nav>
-  </div>
-  <!-- fin nav-->
+    <!--NavBarre-->
+    <div class="sticky-top">
+        <nav class="navbar navbar-dark bg-dark">
+            <div class="container justify-content-between">
+                <div class="d-none d-lg-block">
+                    <a href="./index.php">
+                        <img src="assets/img/2.png" alt="" width="90" height="90">
+                    </a>
+                </div>
+                <div class="navbar navbar-expand-lg">
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar-content"
+                        aria-controls="navbar-content" aria-expanded="false" aria-label="toggle-navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbar-content">
+                        <ul class="navbar-nav">
+                            <li class="nav-item">
+                                <a class="nav-link" href="./index.php">Accueil</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="">Projets</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+        </nav>
+    </div>
+    <!-- fin nav-->
 
+    <!-- section 1-->
+
+    <div class="entreprise">
+        <div class="container-fluid">
+            <div class="row justify-content-end">
+                <div class="d-flex flex-wrap-reverse">
+                    <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+                        <img src="assets/img/photo.png" class="img-fluid" alt="Responsive image">
+                    </div>
+                    <div class="col align-self-center">
+                        <div class="mx-auto  w-75">
+                            <p>
+                                <h2 class="text-center"> Présentation </h2>
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus eos sunt tempore maxime
+                                facilis voluptatem nihil harum porro, obcaecati quisquam rem voluptate doloribus
+                                voluptas aliquam totam dolorum qui repellat et!
+                                lore
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos ab cumque ipsam ad nam
+                                corrupti debitis labore totam quisquam eos reprehenderit nulla cum non odit velit
+                                voluptatibus, laudantium maxime quod?
+                            </p>
+                            <div class="d-grid gap-2 col-6 mx-auto">
+                                <button class="btn btn-primary" type="button">Télécharger CV</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!--  fin section 1-->
 
 
 
